e34bf9d2e6554b20c4a4464ce57132648a370877
Игорь <hardplayer455@gmail.com>
2023-08-21 14:08:25 +0300

    Export changed pom.xml artifact

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopBlackTest {
+
+    @Test
+    void position() {
+    }
+
+    @Test
+    void way() {
+    }
+
+    @Test
+    void isDiagonal() {
+    }
+
+    @Test
+    void copy() {
+    }
+}
